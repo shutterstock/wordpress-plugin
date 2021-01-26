02f582cce296f1d3b53894cce67ea950cf971dbb
Tim McMackin <tmcmackin@shutterstock.com>
2021-01-26 09:06:48 -0500

    Release 1.3.1 DEVPLAT-2371

--- shutterstock/public/shutterstock-block/build/index.asset.php
+++ shutterstock/public/shutterstock-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f82a7a3f573966b77006471e908ebd58');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'bfe06f10ff22ac5a49cc33160d929da0');
